8d66463081d889c33aec6759ea4dbefa9c571246
Roshan Ganesh <roshanganesh06@hotmail.com>
2026-04-01 00:08:47 -0700

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ export default function Navbar() {
               Menu
             </Link>
             <Link
-              href="/menu"
+              href="/cateringmenu"
               onClick={() => setOpen(false)}
               className="flex justify-end block px-4 py-2 text-gray-700 hover:bg-gray-50"
             >
