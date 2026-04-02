d7b60c725f0b4a130ed50e8a69bb35d5f61c7c9f
Roshan Ganesh <roshanganesh06@hotmail.com>
2026-04-01 23:09:40 -0700

    fixed bug with order page

--- src/app/(site)/order/page.js
+++ src/app/(site)/order/page.js
@@ -2,7 +2,7 @@ import { client } from "@/sanity/lib/client";
 import OrderPage from "@/components/Orderpage";
 
 export default async function Order() {
-  const menu = await client.fetch('*[_type == "menuitem"]{_id, itemname, price, section, modifiers[] {name,options}}');
+  const menu = await client.fetch('*[_type == "menuitem" && catering != true]{_id, itemname, price, section, modifiers[] {name,options}}');
   return (
     <OrderPage menu={menu} />
   );
